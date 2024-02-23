222fd63fa8be9bef2f2c8888fd4cfa205a5623a5
huangelina <137594730+huangelina@users.noreply.github.com>
2024-02-22 22:16:45 -0800

    Time: 336 ms (88.03%) | Memory: 28.9 MB (55.47%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -8,7 +8,6 @@ class Solution(object):
         longest = 0
 
         for num in ordered_nums:
-            # check if its the start of a sequence
             if (num - 1) not in ordered_nums:
                 length = 1
                 while (num + length) in ordered_nums:
