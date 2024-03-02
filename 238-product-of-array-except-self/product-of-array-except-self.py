70539beb5ae6e90584f3a2df8a09c9819bc52993
huangelina <137594730+huangelina@users.noreply.github.com>
2024-03-01 23:48:25 -0800

    Time: 155 ms (93.21%) | Memory: 18.5 MB (94.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -9,7 +9,7 @@ class Solution(object):
         for i in range(1, len(nums)):
             result[i] = result[i-1] * nums[i-1]
         postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums) -1, -1, -1):
             result[i] *= postfix
             postfix *= nums[i]
         return result
